02a6dc9b617bc8d5b97632e5871f2daafd4cbd75
GH-00 <ghkim7319@gmail.com>
2024-06-02 16:14:51 +0900

    [Setting]Initial Setting

---
+++ src/main/java/com/example/SomeOne/SomeOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.SomeOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeOneApplication.class, args);
+	}
+
+}
