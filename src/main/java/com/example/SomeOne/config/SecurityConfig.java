424c16158c5ebaf31063c91eee019b4f55dd2f05
GH-00 <ghkim7319@gmail.com>
2024-09-08 20:43:49 +0900

    [Add] SecurityConfig

--- src/main/java/com/example/SomeOne/config/SecurityConfig.java
+++ src/main/java/com/example/SomeOne/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/api/travel-records/**").permitAll() // 인증 없이 접근 허용
-                                .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                                .requestMatchers("/**").permitAll()
                 )
                 .httpBasic(Customizer.withDefaults()) // 기본 HTTP 인증 활성화
                 .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화 (필요에 따라 조정)
