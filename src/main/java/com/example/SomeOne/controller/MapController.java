7b0b970d2b91df602184b6eff839e2db3f7ba51f
GH-00 <ghkim7319@gmail.com>
2024-09-08 21:59:06 +0900

    [fix] island reviews dto fix

--- src/main/java/com/example/SomeOne/controller/MapController.java
+++ src/main/java/com/example/SomeOne/controller/MapController.java
@@ -1,4 +1,28 @@
-package com.example.SomeOne.controller;
-
-public class MapController {
-}
+//package com.example.SomeOne.controller;
+//
+//import com.example.SomeOne.dto.TravelRecords.Response.TravelRecordResponse;
+//import com.example.SomeOne.service.MapService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/maps")
+//@RequiredArgsConstructor
+//public class MapController {
+//
+//    private final MapService mapService;
+//
+//    // 여행 장소 마커를 지도에 표시하기 위한 API
+//    @GetMapping("/travel-records")
+//    public List<TravelRecordResponse> getTravelRecordLocations() {
+//        return mapService.getTravelRecordLocations();
+//    }
+//
+//    // 특정 키워드로 식당 및 숙박 검색하기 위한 API
+//    @GetMapping("/places")
+//    public String searchPlaces(@RequestParam String keyword) {
+//        return mapService.searchPlaces(keyword);
+//    }
+//}
