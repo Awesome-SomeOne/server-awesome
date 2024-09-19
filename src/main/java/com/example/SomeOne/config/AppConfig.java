67fccfdac61f0543db5fa6eadb7f1fbb9adb79bf
GH-00 <ghkim7319@gmail.com>
2024-09-19 21:22:29 +0900

    [fix] travel Records -image urls

--- src/main/java/com/example/SomeOne/config/AppConfig.java
+++ src/main/java/com/example/SomeOne/config/AppConfig.java
@@ -1,14 +1,32 @@
 package com.example.SomeOne.config;
 
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+import java.io.IOException;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                if (response.getStatusCode() != HttpStatus.UNAUTHORIZED) {
+                    super.handleError(response);
+                }
+            }
+        });
+        return restTemplate;
     }
 }
