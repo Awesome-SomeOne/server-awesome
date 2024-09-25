1b867f0c64dcb424cf682a8c1e830ec0b63c0691
chanhee01 <joch4267@naver.com>
2024-09-25 13:17:10 +0900

    [feat] 인기 장소 검색 시 찜 여부 조회

--- src/main/java/com/example/SomeOne/dto/Businesses/response/GetLandmarkResponse.java
+++ src/main/java/com/example/SomeOne/dto/Businesses/response/GetLandmarkResponse.java
@@ -16,5 +16,6 @@ public class GetLandmarkResponse {
     private String xAddress;
     private String yAddress;
     private String imgUrl;
+    private Boolean status;
     private List<ReviewResponse> reviews;
 }
